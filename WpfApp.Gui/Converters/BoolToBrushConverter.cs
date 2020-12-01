436d942633ca0ef9de5945d1e31c2ed9e3efa845
Federico Barresi <federico.barresi@evopro-ag.de>
2020-12-01 23:42:21 +0100

    added some basic converters

---
+++ WpfApp.Gui/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp.Gui.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public Brush IfTrue { get; set; } = Brushes.Green;
+        public Brush IfFalse { get; set; } = Brushes.OrangeRed;
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool) value ? IfTrue : IfFalse;
+            }
+
+            return DependencyProperty.UnsetValue;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
