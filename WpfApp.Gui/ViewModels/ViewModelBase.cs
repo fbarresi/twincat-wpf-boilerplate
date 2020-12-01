c3d8e1e6286d74e3eef21e7bc42a21238194d450
Federico Barresi <federico.barresi@evopro-ag.de>
2020-12-01 11:43:40 +0100

    added vm base and reactive ui

---
+++ WpfApp.Gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
+using DynamicData.Annotations;
+using ReactiveUI;
+using Serilog;
+using WpfApp.Interfaces.Commons;
+
+namespace WpfApp.Gui.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject, IDisposable, IInitializable
+    {
+        protected CompositeDisposable Disposables = new CompositeDisposable();
+        private bool disposed;
+        private string title;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (value == title) return;
+                title = value;
+                raisePropertyChanged();
+            }
+        }
+
+        protected ILogger Logger { get; } = Log.Logger;
+
+        public virtual void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public abstract void Init();
+
+        [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId = "Disposables")]
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            Disposables?.Dispose();
+            Disposables = null;
+
+            disposed = true;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        // ReSharper disable once InconsistentNaming
+        protected void raisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        ~ViewModelBase()
+        {
+            Dispose(false);
+        }
+    }
+}
